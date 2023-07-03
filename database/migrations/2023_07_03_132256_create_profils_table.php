83b27712e8000ad89bb9feb26b0a2a2ad80d514e
metus01 <carnelmetus@gmail.com>
2023-07-03 14:44:05 +0100

    Happy Hour , une p'tite paupause git add .

--- database/migrations/2023_07_03_132256_create_profils_table.php
+++ database/migrations/2023_07_03_132256_create_profils_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,6 +14,11 @@ return new class extends Migration
     {
         Schema::create('profils', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->nullable();
+            $table->string('sexe')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->mediumText('bio')->nullable();
+            $table->string('path')->nullable();
             $table->timestamps();
         });
     }
