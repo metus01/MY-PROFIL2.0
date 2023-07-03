51fe42929eebb09e2710e6aec2995df1cbd7834a
metus01 <carnelmetus@gmail.com>
2023-07-03 14:39:15 +0100

    la bd étant ok , j'peux prendre la pause

--- database/migrations/2023_07_03_132515_create_comments_table.php
+++ database/migrations/2023_07_03_132515_create_comments_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Profil;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Profil::class);
+            $table->mediumText('content');
             $table->timestamps();
         });
     }
--- database/migrations/2023_07_03_132735_create_likes_table.php
+++ database/migrations/2023_07_03_132735_create_likes_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Profil;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Profil::class);
+            $table->boolean('is_liked')->default(false);
+            $table->boolean('is_loved')->default(false);
             $table->timestamps();
         });
     }
