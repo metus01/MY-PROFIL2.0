d03ac9071b76f8ddd0f8a39b8d4758253b989f95
metus01 <carnelmetus@gmail.com>
2023-07-04 08:29:08 +0100

    Matingit add . On poursuit

--- resources/views/livewire/create-profil-component.blade.php
+++ resources/views/livewire/create-profil-component.blade.php
@@ -1,3 +1,12 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
-</div>
+<form action="">
+    <label for="sexe" class="form-label">Sexe</label>
+    <select class="form-select">
+        <option value="M">Masculin</option>
+        <option value="F">Féminin</option>
+        <option value="Perso">Personnalisé</option>
+    </select>
+    <label for="phone_number" class="form-label">Phone Number</label>
+    <input type="tel" name="" id="" class="form-control">
+    <label for="bio" class="form-label">Votre Bio</label>
+    in
+</form>
